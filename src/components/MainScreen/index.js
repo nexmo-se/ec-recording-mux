a231c941971cdc80ab74801329d27179ba19ad23
iujie <iujie.lim@vonage.com>
2023-03-28 23:47:00 +0800

    adjust video layout

--- src/components/MainScreen/index.js
+++ src/components/MainScreen/index.js
@@ -7,6 +7,7 @@ import styles from './styles.module.css'
 import { Tooltip, Typography } from "@mui/material";
 import useWebsocket from "../../hooks/useWebsocket";
 import { useAppState } from "../../state";
+import debounce from "lodash/debounce";
 
 export default function MainScreen() {
     const [localParticipant, setLocalParticipant] = useState(null);
@@ -44,10 +45,73 @@ export default function MainScreen() {
 
     }, [ecRender, roomName, initializeWebsocket, isWebsocketConnecting, websocket ])
 
+    function recalculateLayout() {
+      const gallery = document.getElementById("gallery");
+      const aspectRatio = 16 / 9;
+      const screenWidth = document.body.getBoundingClientRect().width;
+      const screenHeight = document.body.getBoundingClientRect().height - 120;
+      const videoCount = gallery.getElementsByTagName("video").length;
+    
+      // or use this nice lib: https://github.com/fzembow/rect-scaler
+      function calculateLayout( containerWidth,  containerHeight, videoCount,  aspectRatio) {
+        let bestLayout = {
+          area: 0,
+          cols: 0,
+          rows: 0,
+          width: 0,
+          height: 0
+        };
+    
+        // brute-force search layout where video occupy the largest area of the container
+        for (let cols = 1; cols <= videoCount; cols++) {
+          const rows = Math.ceil(videoCount / cols);
+          const hScale = containerWidth / (cols * aspectRatio);
+          const vScale = containerHeight / rows;
+          let width;
+          let height;
+          if (hScale <= vScale) {
+            width = Math.floor(containerWidth / cols) - 10;
+            height = Math.floor(width / aspectRatio) - 10;
+          } else {
+            height = Math.floor(containerHeight / rows) - 10;
+            width = Math.floor(height * aspectRatio) - 10;
+          }
+          const area = width * height;
+          if (area > bestLayout.area) {
+            bestLayout = {
+              area,
+              width,
+              height,
+              rows,
+              cols
+            };
+          }
+        }
+        return bestLayout;
+      }
+    
+      const { width, height, cols } = calculateLayout(
+        screenWidth,
+        screenHeight,
+        videoCount,
+        aspectRatio
+      );
+    
+      gallery.style.setProperty("--width", width + "px");
+      gallery.style.setProperty("--height", height + "px");
+      gallery.style.setProperty("--cols", cols + "");
+    }
+
+    const debouncedRecalculateLayout = debounce(recalculateLayout, 50);
+
+    useEffect(() => {
+      debouncedRecalculateLayout()
+    }, [participants, localParticipant])
+
     return(
         <div>
           <h4 className={styles.roomTitle}>Space: {roomName}</h4>
-            <div className={styles.gridContainer}>
+            <div className={styles.gridContainer} id="gallery">
               {localParticipant && (
                   <Participant
                   key={localParticipant.connectionId}
